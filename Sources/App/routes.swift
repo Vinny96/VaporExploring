5a38d16814b4f7523439044a345d184d5d9b3301
Vinojen Gengatharan <gengatharanvinojen@gmail.com>
2021-04-19 12:49:52 -0400

    Initial Commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,31 @@ func routes(_ app: Application) throws {
     app.get("hello") { req -> String in
         return "Hello, world!"
     }
+    
+    app.get("hello", "vapor"){ req -> String in
+        return "Hello Vapor!"
+    }
+    
+    app.get("hello", ":name"){req -> String in
+        guard let name = req.parameters.get("name") else{
+            throw Abort(.internalServerError)
+        }
+        return "Hello \(name)"
+    }
+    
+    app.post("info"){ req -> InfoResponse in
+        let data = try req.content.decode(InfoData.self)
+        return InfoResponse(request: data)
+    }
+}
+
+struct InfoData : Content
+{
+    let name : String
+    let character : String
+}
+
+struct InfoResponse : Content
+{
+    let request : InfoData
 }
